7cf594d8eea0b2d7e6a0bf633e78c1fb330930de
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-19 22:22:53 +0200

    Update encapsulation01b_updated.cpp

--- source_code/encapsulation01b_updated.cpp
+++ source_code/encapsulation01b_updated.cpp
@@ -11,7 +11,7 @@ public:
     unsigned short int a;
     unsigned short int b;
 
-    PR(short int input1, short int input2) :a(input1), b(input2){};
+    PR(unsigned short int input1, unsigned short int input2) :a(input1), b(input2){};
     int sum() { return a + b; }
 };
 
