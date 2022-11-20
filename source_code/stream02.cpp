97a516c913bd875e45e28fbec08b1be16feeedb6
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-11-20 13:52:09 +0200

    Create stream02.cpp

---
+++ source_code/stream02.cpp
@@ -0,0 +1,27 @@
+// stream02.cpp
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	// temporary string
+	string text_line;
+
+	// Read from the text file
+	ifstream read_file("my_file.txt");
+
+	// read the file line-by-line
+	// with getline() function
+	while (getline(read_file, text_line)) {
+		cout << text_line << endl;
+	}
+
+	// Close the file
+	read_file.close();
+
+	return 1;
+}
